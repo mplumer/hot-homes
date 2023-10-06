bdf75518ff643321cc2d7cdc90d62123a0490c67
Max Plumer <maxplumer12@gmail.com>
2023-10-06 16:18:18 -0400

    initial commit, fix linting error

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
 export default function Home() {
   return <div>Next JS &amp; WordPress course.</div>;
 }
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      data: "Hello World",
+    },
+  }
+}
