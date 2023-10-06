343637461d935608bf985141ac9b98fe76ed4e08
Max Plumer <maxplumer12@gmail.com>
2023-10-06 19:27:08 -0400

    Cover component, tailwind styling, next image

---
+++ components/Cover/Cover.js
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+
+export const Cover = ({children, background}) => {
+    return <div className="h-screen bg-slate-800 relative min-h-[400] flex justify-center items-center">
+        <Image alt="cover" src={background} fill className="mix-blend-soft-light object-cover" />
+        {children}
+        </div>
+}
