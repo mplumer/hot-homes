6ae5cc8218aeca94336ed3aad734e861a7f338ac
Max Plumer <maxplumer12@gmail.com>
2023-10-08 20:52:25 -0400

    pages, child pages, dynamic paths

---
+++ pages/[...slug].js
@@ -0,0 +1,37 @@
+import client from 'client'
+import {gql} from '@apollo/client'
+
+export default function Page(props) {
+    console.log("PROPS", props)
+    return <div>Page</div>
+}
+
+export const getStaticProps = async () => {
+    return {
+        props: {},
+    }
+}
+
+export const getStaticPaths = async () => {
+    const { data } = await client.query({
+        query: gql`
+            query AllPagesQuery {
+                pages {
+                    nodes {
+                        uri
+                    }
+                }
+            }
+        `,
+    });
+
+    return {
+        paths: data.pages.nodes.filter(page => page.uri !== "/").map((page) => ({
+            params: {
+                slug: page.uri.substring(1, page.uri.length - 1).split("/"),
+            },
+        })),
+        fallback: false,
+    };
+};
+
