4845293fbaba70ce348db32257fd9c8d8f21833c
Max Plumer <maxplumer12@gmail.com>
2023-10-08 20:07:10 -0400

    dynamically render heading attributes

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,15 +1,29 @@
 import { Cover } from "components/Cover";
+import { Heading } from "components/Heading";
 
 export const BlockRenderer = ({ blocks }) => {
-    return blocks.map((block) => {
-        switch (block.name) {
-            case "core/cover": {
-                return <Cover key={block.id} background={block.attributes.url}>core cover</Cover>
-            }
-            default:
-                return null;
-        }
+  return blocks.map((block) => {
+    switch (block.name) {
+      case "core/heading": {
+        return (
+          <Heading
+            key={block.id}
+            level={block.attributes.level}
+            content={block.attributes.content}
+            textAlign={block.attributes.textAlign}
+          />
+        );
+      }
+      case "core/cover": {
+        console.log("block", block);
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        );
+      }
+      default:
+        return null;
     }
-    )
-
-}
+  });
+};
--- components/Cover/Cover.js
+++ components/Cover/Cover.js
@@ -1,8 +1,8 @@
 import Image from 'next/image'
 
 export const Cover = ({children, background}) => {
-    return <div className="h-screen bg-slate-800 relative min-h-[400] flex justify-center items-center">
+    return <div className="h-screen text-white bg-slate-800 relative min-h-[400] flex justify-center items-center">
         <Image alt="cover" src={background} fill className="mix-blend-soft-light object-cover" />
-        {children}
+        <div className='max-w-5xl z-10'>{children}</div>
         </div>
 }
