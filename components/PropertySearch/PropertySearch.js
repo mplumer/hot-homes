5108a738c64d4728d11d171b059faf14f930cf3a
Max Plumer <maxplumer12@gmail.com>
2023-10-09 20:02:07 -0400

    create api route for property search

--- components/PropertySearch/PropertySearch.js
+++ components/PropertySearch/PropertySearch.js
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 export const PropertySearch = () => {
+    useEffect(() => {
+        const search = async () => {
+            const response = await fetch("/api/search");
+            const data = await response.json();
+            console.log("Search data ", data);
+        };
+        search();
+    }, []);
     return (
         <div>
         <h1>Property Search</h1>
